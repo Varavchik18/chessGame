83dfe5fdf0094f6eec9c58c3ad423e5c4f1c6a65
varavchik18 <pashavarava1806@gmail.com>
2024-05-24 12:39:19 +0300

    Implemented visualisation of a pieces. Rendered them on the board in correct places to be

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
@@ -20,9 +9,44 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8,8];
+
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Board.Initial(), Player.White);
+            DrawBoard(gameState.Board);
+        }
+
+
+        private void InitializeBoard()
+        {
+            for (int row=0;row<8; row++)
+            {
+                for (int col=0; col<8; col++)
+                {
+                    Image image = new Image();
+                    pieceImages[row,col] = image;
+
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int row =0; row <8; row++)
+            {
+                for (int col =0; col<8; col++)
+                {
+                    Piece piece = board[row, col];
+                    pieceImages[row, col].Source = Images.GetImage(piece); 
+                }
+            }
         }
     }
 }
